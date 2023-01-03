41f4f806a44444fb180addb57516044c0bd94ce4
adarshpv9746 <adarshpv9@gmail.com>
2023-01-03 16:34:39 +0530

    added firefox to browsers

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,26 +52,36 @@ export const config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
+    capabilities: [
+    //     {
     
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-            args: [//'headless',
-            'disable-gpu',
-            //'start-fullscreen'
-        ]
-        },
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+    //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    //     // grid with only 5 firefox instances available you can make sure that not more than
+    //     // 5 instances get started at a time.
+    //     maxInstances: 5,
+    //     //
+    //     browserName: 'chrome',
+    //     'goog:chromeOptions': {
+    //         args: [//'headless',
+    //         'disable-gpu',
+    //         //'start-fullscreen'
+    //     ]
+    //     },
+    //     acceptInsecureCerts: true
+    //     // If outputDir is provided WebdriverIO can capture driver session logs
+    //     // it is possible to configure which logTypes to include/exclude.
+    //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+    //     // excludeDriverLogs: ['bugreport', 'server'],
+    // }
+        {   maxInstances: 1,
+            browserName: 'chrome',
+            acceptInsecureCerts: true
+        }
+        // {   maxInstances: 1,
+        //     browserName: 'firefox',
+        //     acceptInsecureCerts: true
+        // }
+    ],
     //
     // ===================
     // Test Configurations
