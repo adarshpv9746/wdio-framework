bcb6606aec43084ab20567cd5bc6aafc5db55457
adarshpv9746 <adarshpv9@gmail.com>
2023-03-06 15:30:56 +0530

    a

--- update-chrome.sh
+++ update-chrome.sh
@@ -3,4 +3,4 @@ sudo apt-get --only-upgrade install google-chrome-stable
 sudo npm install @wdio/allure-reporter --save-dev
 npm install chromedriver --save-dev
 # if you have Chrome 108 installed on your machine do
-npm install chromedriver@108 --save-dev
+npm install chromedriver@108 --save-dev 
